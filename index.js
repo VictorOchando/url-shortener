1208cae4410410dade3b52b4a0df6d95766f429e
Victor oc <ochando3d@gmail.com>
2021-08-09 19:27:10 +0100

    mongoose setup + DB connection

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
+
+const port = process.env.PORT;
+const DB_URI = process.env.DB_URI;
+
+mongoose.connect(
+    DB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    () => {
+        console.log("DB connected");
+    }
+);
 
 app.get("/short", (req, res) => {
     res.send("This is the /short url");
 });
 
-app.listen(process.env.PORT, () =>
-    console.log("Server listening on port 3000")
-);
+app.listen(port, () => console.log("Server listening on port 3000"));
