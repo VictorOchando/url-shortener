e167dccfef9376dfd907aab0ad89d50086d05939
Victor oc <ochando3d@gmail.com>
2021-08-09 19:07:41 +0100

    dotenv setup

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.get("/short", (req, res) => {
     res.send("This is the /short url");
 });
 
-app.listen(3000, () => console.log("Server listening on port 3000"));
+app.listen(process.env.PORT, () =>
+    console.log("Server listening on port 3000")
+);
